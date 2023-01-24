c5491d81014c90f6039297146b37e2af49dcd9a0
NAVEEDKHAN88 <md.naveedpathan88@gmail.com>
2023-01-25 03:02:15 +0530

    test cases added for validate last name

--- src/test/java/UserValidaterTest.java
+++ src/test/java/UserValidaterTest.java
@@ -4,15 +4,15 @@ import org.junit.Test;
 
 public class UserValidaterTest {
     @Test
-    public void givenFirstName_WhenProper_ShouldReturn_True() {
+    public void givenLastName_WhenProper_ShouldReturn_True() {
         UserValidater userValidater = new UserValidater();
-        boolean result = userValidater.validateFirstName("Naveed");
+        boolean result = userValidater.validateLastName("Khan");
         Assert.assertEquals(true,result);
     }
     @Test
-    public void givenFirstName_WhenNotProper_ShouldReturn_False() {
+    public void givenLastName_WhenNotProper_ShouldReturn_False() {
         UserValidater userValidater = new UserValidater();
-        boolean result = userValidater.validateFirstName("Na");
+        boolean result = userValidater.validateLastName("Kh");
         Assert.assertEquals(false,result);
     }
 }
