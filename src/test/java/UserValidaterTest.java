847c078ddeee7bef5b687a9189755e093722a611
NAVEEDKHAN88 <md.naveedpathan88@gmail.com>
2023-01-25 03:21:41 +0530

    Added test cases for validate password

--- src/test/java/UserValidaterTest.java
+++ src/test/java/UserValidaterTest.java
@@ -4,15 +4,15 @@ import org.junit.Test;
 
 public class UserValidaterTest {
     @Test
-    public void givenPhone_WhenProper_ShouldReturn_True() {
+    public void givenPassword_WhenProper_ShouldReturn_True() {
         UserValidater userValidater = new UserValidater();
-        boolean result = userValidater.validatePhone("918867866123");
+        boolean result = userValidater.validatePassword("1234AA5678");
         Assert.assertEquals(true,result);
     }
     @Test
-    public void givenPhone_WhenNotProper_ShouldReturn_False() {
+    public void givenPassword_WhenNotProper_ShouldReturn_False() {
         UserValidater userValidater = new UserValidater();
-        boolean result = userValidater.validatePhone("8867866123");
+        boolean result = userValidater.validatePassword("123asdfgh");
         Assert.assertEquals(false,result);
     }
 }
